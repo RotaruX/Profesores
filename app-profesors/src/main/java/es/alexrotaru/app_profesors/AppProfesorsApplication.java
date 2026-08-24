04d446aa60e369fd7969579ca2b7479c65e1d6e3
RotaruX <rotarualex1612@gmail.com>
2026-08-24 12:02:12 +0200

    primer commit

---
+++ app-profesors/src/main/java/es/alexrotaru/app_profesors/AppProfesorsApplication.java
@@ -0,0 +1,13 @@
+package es.alexrotaru.app_profesors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppProfesorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppProfesorsApplication.class, args);
+	}
+
+}
---
+++ app-profesors/src/test/java/es/alexrotaru/app_profesors/AppProfesorsApplicationTests.java
@@ -0,0 +1,13 @@
+package es.alexrotaru.app_profesors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppProfesorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
